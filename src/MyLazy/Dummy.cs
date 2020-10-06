9bc21d3b218abb73f4ccc90bfdf054552536e992
hevelmann <jan.gruenklee@me.com>
2020-10-06 12:09:05 +0200

    added code to reproduce the issue

---
+++ src/MyLazy/Dummy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyLazy
+{
+    public class Dummy
+    {
+
+        public void DoSomething()
+        {
+            Console.WriteLine("do something");
+        }
+
+    }
+}
